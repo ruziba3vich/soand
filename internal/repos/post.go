30307fafe3062cf61978ede325cf3bb1bb0360cb
ruziba3vich <rruzibayevich@gmail.com>
2025-02-24 02:23:58 +0500

    feat post_service_interface: added interface for posts service

---
+++ internal/repos/post.go
@@ -0,0 +1,19 @@
+package repos
+
+import (
+	"context"
+
+	"github.com/ruziba3vich/soand/internal/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// PostServiceInterface defines all methods for the PostService
+type IPostService interface {
+	CreatePost(ctx context.Context, post *models.Post, deleteAfter int) (primitive.ObjectID, error)
+	DeletePost(ctx context.Context, id primitive.ObjectID) error
+	EnsureTTLIndex(ctx context.Context) error
+	GetAllPosts(ctx context.Context, page int64, pageSize int64) ([]models.Post, error)
+	GetPost(ctx context.Context, id primitive.ObjectID) (*models.Post, error)
+	UpdatePost(ctx context.Context, id primitive.ObjectID, updaterID primitive.ObjectID, update bson.M) error
+}
