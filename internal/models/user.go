51725b90e942146568016af6828f60fd028c3d89
ruziba3vich <rruzibayevich@gmail.com>
2025-03-04 14:58:17 +0500

    fix models: updated required fields in registration

--- internal/models/user.go
+++ internal/models/user.go
@@ -4,10 +4,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID            primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Fullname      string             `json:"full_name" bson:"full_name"`
-	Phone         string             `json:"phone" bson:"phone" binding:"required"`
+	Fullname      string             `json:"full_name" bson:"full_name" binding:"required"`
+	Phone         string             `json:"phone" bson:"phone"`
 	Username      string             `json:"username" bson:"username" binding:"required"`
-	Password      string             `json:"password" bson:"password"`
+	Password      string             `json:"password" bson:"password" binding:"required"`
 	Bio           string             `json:"bio" bson:"bio"`
 	Status        string             `json:"status" bson:"status"`
 	ProfilePics   []string           `json:"profile_pics" bson:"profile_pics"`
