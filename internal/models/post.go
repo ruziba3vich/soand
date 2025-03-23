4504e2fad2479ef77690a5b2daeb4e39e96af756
ruziba3vich <rruzibayevich@gmail.com>
2025-03-23 22:01:17 +0500

    fix user_model: posts and comments' owner data is retreavable easily now

--- internal/models/post.go
+++ internal/models/post.go
@@ -7,11 +7,13 @@ import (
 )
 
 type Post struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`                // MongoDB ObjectID
-	CreatorId   primitive.ObjectID `bson:"creator_id,omitempty" json:"creator_id"` // Creator id
-	Pictures    []string           `bson:"pictures" json:"picture"`                // Image URLs or file path
-	Tags        []string           `bson:"tags" json:"tags"`                       // List of tags
-	Description string             `bson:"description" json:"description"`         // Post description
-	CreatedAt   time.Time          `bson:"created_at" json:"created_at"`           // Timestamp
-	DeleteAt    time.Time          `bson:"delete_at" json:"delete_at"`             // Field for automatic deletion
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`                // MongoDB ObjectID
+	CreatorId       primitive.ObjectID `bson:"creator_id,omitempty" json:"creator_id"` // Creator id
+	Pictures        []string           `bson:"pictures" json:"picture"`                // Image URLs or file path
+	Tags            []string           `bson:"tags" json:"tags"`                       // List of tags
+	Description     string             `bson:"description" json:"description"`         // Post description
+	CreatedAt       time.Time          `bson:"created_at" json:"created_at"`           // Timestamp
+	DeleteAt        time.Time          `bson:"delete_at" json:"delete_at"`             // Field for automatic deletion
+	OwnerFullname   string             `bson:"owner_full_name" json:"owner_full_name"`
+	OwnerProfilePic string             `bson:"owner_profile_pic" json:"owner_profile_pic"`
 }
