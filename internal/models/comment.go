d469acdaeaed3c103c80146a7d215729023d28e2
ruziba3vich <rruzibayevich@gmail.com>
2025-04-19 03:23:08 +0500

    fix models: change reactions

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -7,15 +7,15 @@ import (
 )
 
 type Comment struct {
-	ID              primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	UserID          primitive.ObjectID `json:"user_id" bson:"user_id"`
-	PostID          primitive.ObjectID `json:"post_id" bson:"post_id"`
-	Text            string             `json:"text,omitempty" bson:"text,omitempty"`
-	VoiceMessage    string             `json:"voice_message,omitempty" bson:"voice_message,omitempty"`
-	Pictures        []string           `json:"pictures,omitempty" bson:"pictures,omitempty"`
-	ReplyTo         primitive.ObjectID `json:"reply_to,omitempty" bson:"reply_to"`
-	OwnerFullname   string             `bson:"owner_full_name" json:"owner_full_name"`
-	OwnerProfilePic string             `bson:"owner_profile_pic" json:"owner_profile_pic"`
-	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
-	Reactions       map[string]int     `json:"reactions" bson:"reactions"`
+	ID              primitive.ObjectID              `json:"id,omitempty" bson:"_id,omitempty"`
+	UserID          primitive.ObjectID              `json:"user_id" bson:"user_id"`
+	PostID          primitive.ObjectID              `json:"post_id" bson:"post_id"`
+	Text            string                          `json:"text,omitempty" bson:"text,omitempty"`
+	VoiceMessage    string                          `json:"voice_message,omitempty" bson:"voice_message,omitempty"`
+	Pictures        []string                        `json:"pictures,omitempty" bson:"pictures,omitempty"`
+	ReplyTo         primitive.ObjectID              `json:"reply_to,omitempty" bson:"reply_to"`
+	OwnerFullname   string                          `bson:"owner_full_name" json:"owner_full_name"`
+	OwnerProfilePic string                          `bson:"owner_profile_pic" json:"owner_profile_pic"`
+	CreatedAt       time.Time                       `json:"created_at" bson:"created_at"`
+	Reactions       map[string][]primitive.ObjectID `json:"reactions" bson:"reactions"`
 }
