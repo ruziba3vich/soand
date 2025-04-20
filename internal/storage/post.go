352b16035dfa7eb82bdc007043c5109255959f71
ruziba3vich <rruzibayevich@gmail.com>
2025-04-21 00:30:44 +0500

    fix storage: remove business logic from storage

--- internal/storage/post.go
+++ internal/storage/post.go
@@ -216,17 +216,6 @@ func (s *Storage) SearchPostsByTitle(ctx context.Context, query string, page, pa
 }
 
 func (s *Storage) LikeOrDislikePost(ctx context.Context, userId primitive.ObjectID, postId primitive.ObjectID, count int) error {
-
-	if count == 1 {
-		if err := s.likes_storage.LikePost(ctx, userId, postId); err != nil {
-			return err
-		}
-	} else {
-		if err := s.likes_storage.DislikePost(ctx, userId, postId); err != nil {
-			return err
-		}
-	}
-
 	filter := bson.M{"_id": postId}
 	update := bson.M{"$inc": bson.M{"likes": count}}
 
