1182d2c948ae4815b9ceda86c53833a5f1c5cb0c
ruziba3vich <rruzibayevich@gmail.com>
2025-04-25 18:45:34 +0500

    feat: add single user data updater method

--- internal/repos/user.go
+++ internal/repos/user.go
@@ -13,13 +13,14 @@ type (
 		DeleteUser(context.Context, primitive.ObjectID) error
 		GetUserByID(context.Context, primitive.ObjectID) (*models.User, error)
 		GetUserByUsername(context.Context, string) (*models.User, error)
-		UpdateFullname(context.Context, primitive.ObjectID, string) error
+		// UpdateFullname(context.Context, primitive.ObjectID, string) error
+		UpdateUser(context.Context, primitive.ObjectID, *models.UserUpdate) error
 		UpdatePassword(context.Context, primitive.ObjectID, string, string) error
 		UpdateUsername(context.Context, primitive.ObjectID, string) error
 		ValidateJWT(string) (string, error)
 		LoginUser(context.Context, string, string) (string, error)
-		ChangeProfileVisibility(context.Context, primitive.ObjectID, bool) error
-		SetBio(context.Context, primitive.ObjectID, string) error
+		// ChangeProfileVisibility(context.Context, primitive.ObjectID, bool) error
+		// SetBio(context.Context, primitive.ObjectID, string) error
 		SetBackgroundPic(context.Context, primitive.ObjectID, string) error
 		AddNewProfilePicture(context.Context, primitive.ObjectID, string) error
 		DeleteProfilePicture(context.Context, primitive.ObjectID, string) error
