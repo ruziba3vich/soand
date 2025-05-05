a1238a9668b50962077296146b44029b8925233d
ruziba3vich <rruzibayevich@gmail.com>
2025-05-05 15:49:10 +0500

    fix: make injection to comment storage

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -142,7 +142,7 @@ func Run(ctx context.Context, logger *log.Logger) error {
 	}
 
 	comments_storage := storage.NewCommentStorage(comments_collection, user_storage)
-	comments_service := service.NewCommentService(comments_storage, user_storage, redisClient, logger)
+	comments_service := service.NewCommentService(comments_storage, user_storage, file_store_service, redisClient, logger)
 
 	registerar.RegisterCommentRoutes(
 		router,
