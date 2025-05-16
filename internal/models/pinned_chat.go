47c7f87bf54264c5ce22a3789ae7f400b73a7c70
ruziba3vich <rruzibayevich@gmail.com>
2025-05-17 00:48:39 +0500

    feat: add a model to get request to pin a chat

--- internal/models/pinned_chat.go
+++ internal/models/pinned_chat.go
@@ -4,3 +4,8 @@ type PinnedChat struct {
 	ChatId string `json:"chat_id"`
 	Pinned bool   `json:"pinned"`
 }
+
+type PinChatRequest struct {
+	ChatID string `json:"chat_id"`
+	Pin    bool   `json:"pin"`
+}
