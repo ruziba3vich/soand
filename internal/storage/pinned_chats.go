3602cee42a6f5bab894b1d22950f3c7ee0c0ba15
ruziba3vich <rruzibayevich@gmail.com>
2025-05-17 00:26:09 +0500

    feat: create storage for pinned chats

---
+++ internal/storage/pinned_chats.go
@@ -0,0 +1,29 @@
+package storage
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type PinnedChat struct {
+	db *mongo.Collection
+}
+
+func NewPinnedChat(db *mongo.Collection) *PinnedChat {
+	return &PinnedChat{
+		db: db,
+	}
+}
+
+func (p *PinnedChat) SetPinned(ctx context.Context, chatID primitive.ObjectID, pinned bool) error {
+	filter := bson.M{"chat_id": chatID}
+	update := bson.M{"$set": bson.M{"pinned": pinned}}
+
+	opts := options.Update().SetUpsert(true)
+	_, err := p.db.UpdateOne(ctx, filter, update, opts)
+	return err
+}
