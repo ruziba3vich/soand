9e460f29eaedb956352a9badee6fd160060db0fc
ruziba3vich <rruzibayevich@gmail.com>
2025-07-31 18:53:01 +0500

    fix: fix return type of file uploader

--- internal/repos/file_store.go
+++ internal/repos/file_store.go
@@ -1,12 +1,16 @@
 package repos
 
-import "mime/multipart"
+import (
+	"mime/multipart"
+
+	dto "github.com/ruziba3vich/soand/internal/dtos"
+)
 
 type (
 	IFIleStoreService interface {
 		DeleteFile(string) error
 		GetFile(string) (string, error)
-		UploadFile(*multipart.FileHeader) (string, error)
+		UploadFile(*multipart.FileHeader) (*dto.FileObject, error)
 		UploadFileFromBytes([]byte, string) (string, error)
 	}
 )
